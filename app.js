3d185bc50f047b5e273d42f3abb94674a82ad3e9
long.bai <long198519@gmail.com>
2017-01-29 22:09:23 +0100

    tp chapitre 2

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var favicon = require('serve-favicon'); // Charge le middleware de favicon
 
 var app = express();
+
+app.use(morgan('combined')) // Active le middleware de logging
+    .use(express.static(__dirname + '/public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+    .use(favicon(__dirname + '/public/favicon.ico')); // Active la favicon indiquée
+
 
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
@@ -13,8 +20,12 @@ app.get('/sous-sol', function(req, res) {
 });
 
 app.get('/etage/:etagenum/chambre', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
+    res.render('chambre.ejs', {etage: req.params.etagenum});
+});
+
+app.get('/compter/:nombre', function(req, res) {
+    var noms = ['Robert', 'Jacques', 'David'];
+    res.render('page.ejs', {compteur: req.params.nombre, noms: noms});
 });
 
 app.use(function(req, res, next){
